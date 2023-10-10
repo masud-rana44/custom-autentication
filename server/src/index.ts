027b7d4899c5847a7fb0cad9990db54fc9e5aa9d
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-11 03:00:47 +0600

    created user schema & model

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,19 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import * as dotenv from "dotenv";
 
 import mongoose from "mongoose";
 
+dotenv.config();
+
 const app = express();
+
+app.use(cors({ credentials: true }));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
@@ -16,4 +25,11 @@ server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 
-app.use(cors());
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URL);
+mongoose.connection.on("connected", () => {
+  console.log("mongodb connected");
+});
+mongoose.connection.on("error", (err: Error) => console.log(err));
---
+++ server/src/models/userModel.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: true,
+    required: [true, "Email is required"],
+    lowercase: true,
+    unique: true,
+    trim: true,
+    // TODO: email validation
+  },
+  photo: {
+    type: String,
+    default: "default.jpg",
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Confirm password is required"],
+    validate: {
+      validator: function (el: string) {
+        return el === this.password;
+      },
+      message: "Password and confirm password are not same",
+    },
+  },
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
