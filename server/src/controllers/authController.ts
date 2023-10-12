e958a4ad520579d08d304d540e463ee6b3ed9f61
masud-rana44 <mdmasudrana4488@gmail.com>
2023-10-13 02:40:20 +0600

    Implemented user logout API

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -109,3 +109,12 @@ export const login = async (req: Request, res: Response) => {
   // if everything okay, send token to the client
   createSendToken(user, 200, res);
 };
+
+export const logout = (req: Request, res: Response) => {
+  res.cookie("jwt", "loggedout", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ status: "success" });
+};
--- server/src/routers/authRouters.ts
+++ server/src/routers/authRouters.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/authController";
+import { register, login, logout } from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
