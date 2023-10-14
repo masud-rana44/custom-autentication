fb0bcf9680c05d0aacb2798b06128da56a27bb20
JiJetu <jijetu2@gmail.com>
2023-10-14 22:13:25 +0600

    updated login background color

--- client/app/(auth)/layout.tsx
+++ client/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-gray-900 h-full min-h-screen  flex items-center justify-center">
+    <div className="bg-gray-800 h-full min-h-screen  flex items-center justify-center">
       {children}
     </div>
   );
