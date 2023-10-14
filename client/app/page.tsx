4269746694edb16327fe1e499a1167e6e8152bf3
Shaan Mohammad Rizwan <shaanmr07@gmail.com>
2023-10-14 22:30:33 +0600

    feat: Implemented banner section ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div className="text-center font-bold text-5xl text-indigo-700">
-      Home Page
-    </div>
+    <section className="bg-primary-600 text-white py-16">
+      <div className="container mx-auto text-center">
+        <h1 className="text-4xl font-extrabold">Welcome to Our Authentication App</h1>
+        <p className="mt-4 text-lg">Secure your account and access amazing features.</p>
+        <button className="bg-white text-primary-600 hover:bg-primary-700 text-lg font-semibold rounded-full py-2 px-6 mt-8">
+          Get Started
+        </button>
+      </div>
+    </section>
   );
 }
