2ab6dab7ae22e1baa48f4094599f0136579f7ae5
JiJetu <jijetu2@gmail.com>
2023-10-20 11:22:08 +0600

    added login as a route on home page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="text-center font-bold text-5xl text-indigo-700">
-      Home Page
+    <div className="flex justify-between items-center container mx-auto">
+      <div className="flex-1 text-center font-bold text-5xl text-indigo-700">
+        Home Page
+      </div>
+      <div>
+        <Link href="/login" className="bg-indigo-500 text-white text-lg py-2 px-6 rounded-md hover:border-2 hover:border-indigo-900 hover:bg-indigo-700">LogIn</Link>
+      </div>
     </div>
+
   );
 }
